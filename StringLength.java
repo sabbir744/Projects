dc0925ab1dd0676d5d4c48a7771cad132bb89c3c
Sabbir Ahmed <sbbrcse@gmail.com>
2018-03-28 08:37:57 +0000

    Upload New File

---
+++ StringLength.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class StringLength {
+			
+	
+	
+	
+		private static String i = " ";
+		//private static String s = " ";
+	public static void main(String[] args) {
+		
+		
+		System.out.println("enter the string");
+		Scanner input = new Scanner (System.in);
+		
+		i =  input.next();
+		
+		System.out.println(i.length());
+		
+		
+		
+		
+		
+
+	}
+
+}
