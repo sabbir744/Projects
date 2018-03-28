5a70003489fab51f050cd75d8fc36df109b56936
Sabbir Ahmed <sbbrcse@gmail.com>
2018-03-28 08:35:36 +0000

    Upload New File

---
+++ Alphabet.java
@@ -0,0 +1,20 @@
+
+public class Alphabet {
+		
+	private static char ch;
+	public static void main(String[] args) {
+		
+	for(ch = 'A';ch<='Z';ch++)	
+	{
+		System.out.println(ch);
+	}
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
