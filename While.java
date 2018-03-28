16750855bf09ea8d69330ac090119fa19f13a66a
Sabbir Ahmed <sbbrcse@gmail.com>
2018-03-28 08:38:16 +0000

    Upload New File

---
+++ While.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class While {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner (System.in);
+		int i = input.nextInt();
+		
+		
+		do
+		{
+			System.out.println(i);
+			
+		}
+		
+		
+		
+		while(input.nextInt()!=0);
+		{
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
