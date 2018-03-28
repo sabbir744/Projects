dcbbcd37c80282b8fae56d7026860d4649b19801
Sabbir Ahmed <sbbrcse@gmail.com>
2018-03-28 08:36:05 +0000

    Upload New File

---
+++ ForLoop.java
@@ -0,0 +1,23 @@
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0;i<=100;i++)
+		{
+			i=i+1; 
+			System.out.println(i);
+			
+			
+			
+			
+			
+		}
+		
+		
+		
+		
+
+	}
+
+}
