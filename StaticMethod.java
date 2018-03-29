0a74c4ddc704f333fbe2e85a4b032b0e3bef7ae8
Sabbir Ahmed <sbbrcse@gmail.com>
2018-03-29 18:50:39 +0000

    Upload New File

---
+++ StaticMethod.java
@@ -0,0 +1,25 @@
+
+public class StaticMethod {
+		
+	public static void main(String[] args) {
+		
+		display();
+		
+		
+		/*instant call is static with return type with void display method and the main method 
+		with referred by the static method
+		*/
+		
+		
+		
+		
+
+	}
+	
+	static void display()
+	{
+		System.out.println("sabbir Ahmned");
+	}
+	
+
+}
