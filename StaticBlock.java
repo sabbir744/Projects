60b5427980e2297b9699c4cb4533fd6c1373d1ab
Sabbir Ahmed <sbbrcse@gmail.com>
2018-03-29 18:50:17 +0000

    Upload New File

---
+++ StaticBlock.java
@@ -0,0 +1,28 @@
+
+public class StaticBlock {
+	
+		
+	
+	
+	
+
+	
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Ahmed");//next priority main method block
+		
+		
+		
+		
+		
+		
+
+	}
+	static
+	{
+		System.out.println("sabbir"); //first priority static block
+	}
+
+}
