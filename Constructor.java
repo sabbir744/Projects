48e0cd895297d4bbc38f0f5e30b47c8a9019cd18
Sabbir Ahmed <sbbrcse@gmail.com>
2018-03-29 18:49:06 +0000

    Upload New File

---
+++ Constructor.java
@@ -0,0 +1,20 @@
+
+ class Constructor {
+	 
+	 Constructor(String s,int x,int y)
+	 {
+		 s= "sabbir";
+		 System.out.println(s.charAt(2)); // CharAt method  means the index of the string and (int) means the position of index;
+	 }
+
+	public static void main(String[] args) {
+		
+		Constructor m = new Constructor("sabbir", 10, 20);
+		
+		
+		
+		
+
+	}
+
+}
