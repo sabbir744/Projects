e1b9cfbaf169fb39686a3ca85681e7352922856a
Sabbir Ahmed <sbbrcse@gmail.com>
2018-03-29 18:49:29 +0000

    Upload New File

---
+++ JavaMethod.java
@@ -0,0 +1,36 @@
+
+public class JavaMethod {
+			private static int z;
+			//private static int x,y;
+		void run()
+		{
+			System.out.println("Sabbir Ahmed");
+			
+			
+			for(z=1;z<=10;z++) {
+			
+			
+			System.out.println(z);
+			}
+		}
+	
+	
+	
+	
+	
+
+	public static void main(String[] args) {
+		
+		
+		
+		JavaMethod m = new JavaMethod ();
+		m.run();
+	    
+		
+		
+		
+		
+
+	}
+
+}
